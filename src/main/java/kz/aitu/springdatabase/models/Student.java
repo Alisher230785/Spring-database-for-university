7a78281884c02f744487f5323375d5f55c39a1df
alishershayakhmetov <shayakhmetovalisher1@gmail.com>
2024-02-20 12:32:09 +0600

    added student model

---
+++ src/main/java/kz/aitu/springdatabase/models/Student.java
@@ -0,0 +1,18 @@
+package kz.aitu.springdatabase.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String last_name;
+    private int age;
+    private int course;
+    private double gpa;
+}
